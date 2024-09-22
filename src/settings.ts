2a439573163608ebfbad99e7a0a4c9a51af0b424
Preston Kemp <preston@prestonkemp.com>
2024-09-22 16:00:10 -0400

    as

--- src/settings.ts
+++ src/settings.ts
@@ -6,4 +6,4 @@ export const PLATFORM_NAME = 'EnvironmentalMonitoring';
 /**
  * This must match the name of your plugin as defined the package.json
  */
-export const PLUGIN_NAME = 'carbon-dioxide-monitor';
+export const PLUGIN_NAME = 'homebridge-carbondioxide-monitor';
